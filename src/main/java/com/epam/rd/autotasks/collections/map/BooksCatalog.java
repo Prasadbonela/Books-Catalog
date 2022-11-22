2c66fafe70d6f5788c4b33b2b13918a7b329d71c
Iurii Mishcheriakov <eng.syst@gmail.com>
2022-11-22 14:32:24 +0200

    Initial commit

---
+++ src/main/java/com/epam/rd/autotasks/collections/map/BooksCatalog.java
@@ -0,0 +1,86 @@
+package com.epam.rd.autotasks.collections.map;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BooksCatalog /*extends TreeMap<Author, List<Book>>*/ {
+    private static final String EOL = "\n";
+    private Map<Author, List<Book>> catalog;
+
+    public BooksCatalog() {
+        // place your code here
+    }
+
+    public BooksCatalog(Map<Author, List<Book>> catalog) {
+    }
+
+    /**
+     * Returns a List of books of the specified author.
+     *
+     * @param author the author of books to search for.
+     * @return a list of books or {@code null}
+     * if there is no such author in the catalog.
+     */
+    public List<Book> findByAuthor(Author author) {
+        // place your code here
+        return null;
+    }
+
+    /**
+     * @return the string representation of all authors
+     * separated by the current operating system {@code lineSeparator}.
+     */
+    public String getAllAuthors() {
+        // place your code here
+        return null;
+    }
+
+    /**
+     * Searches for pairs of (author, book) by the book title.
+     * The pair must be included in the resulting map if the
+     * book title contains the specified string matched ignore case.
+     * All authors of the book must be specified in the
+     * book authors list.
+     *
+     * @param pattern the string to search for in the book title.
+     * @return the map which contains all found books and their authors.
+     * It must be sorted by titles of books.
+     */
+    public Map<Book, List<Author>> findAuthorsByBookTitle(String pattern) {
+        // place your code here
+        return null;
+    }
+
+    /**
+     * Searches for all books whose genre list contains the specified string.
+     * The book must be included in the resulting list if at least
+     * one genre of the book contains the specified pattern ignoring case.
+     *
+     * @param pattern the string to search for in the book genre list.
+     * @return an authors list of the found books.
+     * @see Book class.
+     */
+    public Set<Book> findBooksByGenre(String pattern) {
+        // place your code here
+        return null;
+    }
+
+    /**
+     * Searches for authors of the specified book.
+     *
+     * @param book the book.
+     * @return a list of authors of the specified book.
+     * @throws NullPointerException if the parameter is {@code null}
+     */
+    public List<Author> findAuthorsByBook(Book book) {
+        // place your code here
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // place your code here
+        return null;
+    }
+}
